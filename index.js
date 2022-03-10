0f0ec3b20891cc606e00dee8d277325a5b9ec8a9
rafael <rafael.souza@agenciaeplus.com.br>
2022-03-10 12:55:13 -0300

    heroku

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+let acertos = 0;
+let errors = 0;
+let money = 17;
+let numberAcertos = [];
+let mostWrongStrategy = [];
+let mostAcertosStrategy = [];
+
+export const app = express();
+
+app.use(express.json());
+
+app.get("/get", (req, res) => {
+  res.send({ acertos, errors, money, numberAcertos, mostAcertosStrategy, mostWrongStrategy });
+});
+
+app.post("/post", (req, res) => {
+  const { body } = req;
+
+  acertos = body.acertos;
+  errors = body.errors;
+  money = body.money;
+  numberAcertos = body.mostWrongStrategy;
+  mostAcertosStrategy = body.mostAcertosStrategy;
+
+  res.send(`tudo ok`);
+});
+
+app.listen(process.env.PORT || 3333, () => console.log("servidor ligado"));
