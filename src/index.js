2f8fd3fc32e83c92639274e8de518d286a39a391
rafael <rafael.souza@agenciaeplus.com.br>
2022-03-10 19:23:01 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
 import express from "express";
 import cors from "cors";
 
-let acertos = 0;
-let errors = 0;
-let money = 17;
-let numberAcertos = [];
-let mostWrongStrategy = [];
-let mostAcertosStrategy = [];
+let infos = {};
 
 export const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/get", (req, res) => {
-  res.send({ acertos, errors, money, numberAcertos, mostAcertosStrategy, mostWrongStrategy });
+  res.send(infos);
 });
 
 app.post("/post", (req, res) => {
   const { body } = req;
 
-  acertos = body.acertos;
-  errors = body.errors;
-  money = body.money;
-  numberAcertos = body.mostWrongStrategy;
-  mostAcertosStrategy = body.mostAcertosStrategy;
-
   console.log(body);
+  infos = body;
 
-  res.send({ acertos, errors, money, numberAcertos, mostAcertosStrategy, mostWrongStrategy });
+  console.log(infos, "post");
+
+  res.send(infos);
 });
 
 app.listen(process.env.PORT || 3333, () => console.log("servidor ligado"));
